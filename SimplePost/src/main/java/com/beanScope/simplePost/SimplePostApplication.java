2cc76a76c39c2d62af4e5c9fd5c8704bb164ac63
ar25029 <ar25029@gmail.com>
2026-08-13 07:35:54 +0530

    Spring Boot- bean scope with singleton and prototype with different DI like constrctor injection and setter injection

--- SimplePost/src/main/java/com/beanScope/simplePost/SimplePostApplication.java
+++ SimplePost/src/main/java/com/beanScope/simplePost/SimplePostApplication.java
@@ -45,8 +45,6 @@ public class SimplePostApplication {
 			}
 			case 3 -> {
 				System.out.println("Exiting...");
-				context.close();
-				sc.close();
 				return;
 			}
 			default -> System.err.println("Wrong Choice!");
